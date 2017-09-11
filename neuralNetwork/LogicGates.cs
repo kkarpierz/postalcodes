7fbd9eba3c0c5d09c1114450203e0348c1acda07
Kuba Skwara <skwaraj@student,agh.edu.pl>
2017-09-11 22:33:49 +0200

    Small corrections

--- neuralNetwork/LogicGates.cs
+++ neuralNetwork/LogicGates.cs
@@ -17,39 +17,14 @@ namespace CSharp_Neural_Network
         {
             var learnRate = 0.2;
 
-            //Console.WriteLine("Training Not:");
-            //Console.WriteLine("=============");
-            //new Trainer(new Network(1, new int[0], 1, learnRate), notCases, CheckCorrect)
-            //    .TrainUntilDone();
-
-            //Console.WriteLine("Training Or:");
-            //Console.WriteLine("============");
-            //new Trainer(Create21Network(), orCases, CheckCorrect)
-            //    .TrainUntilDone();
 
             Console.WriteLine("Training numbers:");
             Console.WriteLine("==============");
             numberNetwork = Create35to10Network();
 
-            //new Trainer(numberNetwork, numCases2, CheckCorrect, testNums).TrainUntilDone();
-            new Trainer(numberNetwork, numCases2, CheckCorrect, testNums).Train(300);
+            new Trainer(numberNetwork, numCases2, CheckCorrect, testNums).TrainUntilDone();
+            //new Trainer(numberNetwork, numCases2, CheckCorrect, testNums).Train(300);
 
-
-
-            //Console.WriteLine("Training And:");
-            //Console.WriteLine("=============");
-            //new Trainer(Create21Network(), andCases, CheckCorrect)
-            //    .TrainUntilDone();
-
-            //Console.WriteLine("Training Nand:");
-            //Console.WriteLine("==============");
-            //new Trainer(Create21Network(), nandCases, CheckCorrect)
-            //    .TrainUntilDone();
-
-            //Console.WriteLine("Training Xor:");
-            //Console.WriteLine("=============");
-            //new Trainer(new Network(2, new []{2} , 1, learnRate), xorCases, CheckCorrect)
-            //    .TrainUntilDone();
         }
 
         public static void Recognize(double[] inputVector = null) {
@@ -78,12 +53,6 @@ namespace CSharp_Neural_Network
 
         static void HL() { Console.WriteLine(new string('=', 0)); }
 
-        public static void TrainNot()
-        {
-            var net = new Network(1, new int[0], 1, 0.5);
-            var trainer = new Trainer(net, notCases, CheckCorrect, notCases);
-            trainer.TrainUntilDone();
-        }
 
         static Network Create21Network()
         {
@@ -108,17 +77,6 @@ namespace CSharp_Neural_Network
         }
 
 
-        static Sample[] notCases = new[]{
-                Sample.Create("0", "1"),
-                Sample.Create("1", "0"),
-            };
-
-        static Sample[] orCases = new[]{
-                Sample.Create("0 0", "0"),
-                Sample.Create("0 1", "1"),
-                Sample.Create("1 0", "1"),
-                Sample.Create("1 1", "1"),
-            };
 
         static Sample[] numCases = new[] {
                 Sample.Create("1 1 1 1 1 1 0 0 0 1 1 0 0 0 1 1 0 0 0 1 1 0 0 0 1 1 0 0 0 1 1 1 1 1 1", "0 0 0 0"),
@@ -200,26 +158,6 @@ namespace CSharp_Neural_Network
             Sample.Create("1 1 1 1 1 1 0 0 0 1 1 0 0 0 1 1 0 0 0 1 1 0 0 0 1 1 0 0 0 1 1 1 1 1 1", "0 0 0 0")
         };
 
-        static Sample[] andCases = new[]{
-                Sample.Create("0 0", "0"),
-                Sample.Create("0 1", "0"),
-                Sample.Create("1 0", "0"),
-                Sample.Create("1 1", "1"),
-            };
-
-        static Sample[] nandCases = new[]{
-                Sample.Create("0 0", "1"),
-                Sample.Create("0 1", "1"),
-                Sample.Create("1 0", "1"),
-                Sample.Create("1 1", "0"),
-            };
-
-        static Sample[] xorCases = new[]{
-                Sample.Create("0 0", "0"),
-                Sample.Create("0 1", "1"),
-                Sample.Create("1 0", "1"),
-                Sample.Create("1 1", "0"),
-            };
 
 
     }
